c1390d34ae78dc133c1426b64bd7e9c8de7929cd
CreativePolyon <64415102+Polyon@users.noreply.github.com>
2021-12-26 17:26:53 +0530

    Update OwlCarousel
    To use owl-carousel you need:
    >> ngx-owl-carousel-o
    >> tslib
    >> BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 // --------------Owl Carousel------------------
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// --------------------------------------------
+// ---------------------------------------------
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
